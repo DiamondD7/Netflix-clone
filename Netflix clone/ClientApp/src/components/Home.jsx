693d91a853e7c6a3e6ba692c58264ea3f09a5e79
diamo <diamo@DESKTOP-7M5FGFJ>
2022-09-09 19:44:43 +1200

    finished the poster of the project

--- Netflix clone/ClientApp/src/components/Home.jsx
+++ Netflix clone/ClientApp/src/components/Home.jsx
@@ -4,8 +4,30 @@ export default function Home() {
     return (
         <>
             <div className="header--poster">
+                <button className="hum--menu">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="menu--icon">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+
+                <img className="netflix--logo" src="https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"></img>
+
+                <div className="search--div">
+                    <button className="search--button">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="search--icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                    </button>
+
+                    <button className="dots--button">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="dots--icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                        </svg>
+                    </button>
+                </div>
+
                 <div className="summary--div">
-                    <button className="summary--buttons">                       
+                    <button className="summary--buttons">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="icon">
                             <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
                         </svg>
@@ -19,11 +41,11 @@ export default function Home() {
                     <div className="poster--summary">
                         <p>Ragnar Lothbrok is determined to sail west to discover new lands and riches despite an intimidating warning from Earl Heraldson.
                             A Viking who is one of the best-known legendary Norse heroes and notorious as the scourge of Anglo-Saxon England and West Francia.
-                        </p>   
+                        </p>
                     </div>
                     <p className="watch--now"><strong>Watch the series now</strong></p>
                 </div>
             </div>
         </>
-        )
+    )
 }
